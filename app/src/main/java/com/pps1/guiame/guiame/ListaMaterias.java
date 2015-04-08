430d1217b5e18977f4c3661a111726e5882b2287
Agustina <agusdenapoli@hotmail.com>
2015-04-08 20:05:48 -0300

    Prueba

--- app/src/main/java/com/pps1/guiame/guiame/ListaMaterias.java
+++ app/src/main/java/com/pps1/guiame/guiame/ListaMaterias.java
@@ -1,20 +1,25 @@
 package com.pps1.guiame.guiame;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-import android.view.View.OnClickListener;
 import java.util.ArrayList;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class ListaMaterias extends ActionBarActivity
+public class ListaMaterias extends Activity
 {
     private ListView listaMaterias;
     ArrayAdapter<String> adaptador;
@@ -66,9 +71,34 @@ public class ListaMaterias extends ActionBarActivity
 
             }
         });
-
     }
 
+    public void getSelected(View view)
+    {
+        int posicion = listaMaterias.getCheckedItemPosition(); //Devuelve la posición del item seleccionado
+        if(posicion<0)
+        {
+            //Si no hay elemento seleccionado...
+            Toast.makeText(this, "No hay elementos seleccionados", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            String itemSeleccionado = listaMaterias.getItemAtPosition(posicion).toString();
+            int posInicio = itemSeleccionado.indexOf(":");
+            int posFin = itemSeleccionado.indexOf("-", posInicio);
+            String aula = itemSeleccionado.substring(posInicio+1,posFin).trim();
+
+            Toast.makeText(this, "AULA " +  aula, Toast.LENGTH_SHORT).show();
+
+            Bundle b = new Bundle();
+            b.putString("elementoSeleccionado", aula);
+
+            Intent intent = new Intent(getApplicationContext(), Prueba.class);
+            intent.putExtras(b);
+
+            startActivity(intent);
+        }
+    }
 
     //Esto tambien deberia ir a Listador pero no se puede pasar el ListView
     public void mostrarItems(ArrayList<String> datos)
@@ -76,6 +106,5 @@ public class ListaMaterias extends ActionBarActivity
         adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datos);
         listaMaterias = (ListView) findViewById(R.id.listaMaterias);
         listaMaterias.setAdapter(adaptador);
-
     }
 }
